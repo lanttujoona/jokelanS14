f1e637500d80665411fe6f780cc7a160221dd01b
Joona Paavola <joona@lanttu.org>
2014-08-01 13:53:41 +0300

    tehtiin kokolevee tempalte

---
+++ page-full.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Template Name: Kokolevee
+*/
+
+<?php get_template_part('parts/header'); ?>
+
+<nav class="navbar navbar-site-title">
+  	<div class="container">
+  		<?php if(have_posts()): while(have_posts()): the_post();?>
+  			<header>
+  				<h2><?php the_title()?></h2>
+  			</header>
+  		<?php endwhile; ?> 
+  		<?php endif;?>
+  	</div>
+    <div class="navbar-box">
+  	</div>
+</nav>
+
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-12">
+      <div id="content" role="main">
+        <?php if(have_posts()): while(have_posts()): the_post();?>
+        <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
+          <?php the_content()?>
+        </article>
+        <?php endwhile; ?> 
+        <?php else: ?>
+        <?php wp_redirect(get_bloginfo('siteurl').'/404', 404); exit; ?>
+        <?php endif;?>
+      </div><!-- /#content -->
+    </div>    
+  </div><!-- /.row -->
+</div><!-- /.container -->
+
+<?php get_template_part('parts/footer'); ?>
