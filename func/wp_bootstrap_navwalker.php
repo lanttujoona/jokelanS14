91e1df8eab6e4d861acd4063586f2ea39cb585fd
Joona Paavola <joona@lanttu.org>
2014-08-30 14:14:57 +0300

    pikkubugi  vielä

--- func/wp_bootstrap_navwalker.php
+++ func/wp_bootstrap_navwalker.php
@@ -88,7 +88,7 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu {
 					$atts['data-hover']		= 'dropdown';
 				}				
 				$atts['data-toggle']	= 'dropdown';
-				$atts['class']			= 'dropdown';
+				$atts['class']			= 'dropdown-toggle';
 				$atts['aria-haspopup']	= 'true';
 			} else {
 				$atts['href'] = ! empty( $item->url ) ? $item->url : '';
